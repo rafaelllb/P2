7ccb595085e4d1b84bdf5b078df1c459c0a571b2
Rafael <rafael.leonardo.brito@gmail.com>
2015-02-04 14:03:01 -0300

    Remoção de lixo da classe main.

--- src/livrariaSI/Main.java
+++ src/livrariaSI/Main.java
@@ -4,13 +4,5 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Aluno a = new Aluno();
-		Funcionario f = new Funcionario();
 		
-		StringBuffer n = new StringBuffer();
-		n.append("Rafael");
-		f.setNome(n);
-		
-		System.out.println(f.getNome()); 
-	}
 }
